b72921229d44407bb5e7e6dc05e6e998355680bc
Maestix <budnikovs@gmail.com>
2025-12-07 14:31:17 +0300

    feat: доработка клиента

--- LoadTester/Program.cs
+++ LoadTester/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using LoadTester;
+
+var sourceToken = new CancellationTokenSource();
+
+using SimpleTcpClient client = new("127.0.0.1", 8080);
+await client.ConnectAsync(sourceToken.Token);
+
+bool isSuccess = await client.SetAsync("key1", Encoding.UTF8.GetBytes("test1 test1"), sourceToken.Token);
+
+if (isSuccess)
+{
+    string? cachedValue = await client.GetAsync("key1", sourceToken.Token);
+
+    Console.WriteLine($"VALUE: {cachedValue}");
+}
+else
+{
+    Console.WriteLine("ERROR SET");
+}
+
+Console.WriteLine("END");
--- LoadTester/SimpleTcpClient.cs
+++ LoadTester/SimpleTcpClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Buffers;
 using System.Buffers.Binary;
 using System.Net.Sockets;
 using System.Text;
@@ -7,99 +8,129 @@ using System.Threading.Tasks;
 
 namespace LoadTester;
 
-public class SimpleTcpClient : IDisposable
+public class SimpleTcpClient(string host, int port) : IDisposable
 {
-    private TcpClient _client = new();
-    private NetworkStream _stream;
-    private readonly string _host;
-    private readonly int _port;
+    private readonly TcpClient _client = new();
+    private NetworkStream? _stream;
+    private readonly string _host = host;
+    private readonly int _port = port;
 
-    public SimpleTcpClient(string host, int port)
-    {
-        _host = host;
-        _port = port;
-    }
-
-    public async Task ConnectAsync()
+    public async Task ConnectAsync(CancellationToken token)
     {
         if (_client.Connected)
             throw new InvalidOperationException("Соединение установлено ранее");
 
-        await _client.ConnectAsync(_host, _port);
+        await _client.ConnectAsync(_host, _port, token);
         _stream = _client.GetStream();
     }
 
-    public async Task SetAsync(string key, byte[] value)
+    public async Task<bool> SetAsync(string key, byte[] value, CancellationToken token)
     {
         if (!_client.Connected || _stream == null)
             throw new InvalidOperationException("Соединение не установлено");
 
-        // Формируем сообщение: SET + key length + key + value length + value
-        var keyBytes = Encoding.UTF8.GetBytes(key);
-        var keyLen = BitConverter.GetBytes(keyBytes.Length);
-        var valueLen = BitConverter.GetBytes(value.Length);
+        byte[] commandBytes = Encoding.UTF8.GetBytes("SET");
+        byte[] spaceSeparator = Encoding.UTF8.GetBytes(" ");
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
 
-        var message = new byte[4 + keyBytes.Length + 4 + value.Length];
+        byte[] message = new byte[commandBytes.Length + spaceSeparator.Length + keyBytes.Length + spaceSeparator.Length + value.Length];
+
         int offset = 0;
 
-        // Команда SET (4 байта)
-        Encoding.UTF8.GetBytes("SET ", 0, 4, message, offset);
-        offset += 4;
+        Buffer.BlockCopy(commandBytes, 0, message, offset, commandBytes.Length);
+        offset += commandBytes.Length;
 
-        // Длина ключа (4 байта)
-        Buffer.BlockCopy(keyLen, 0, message, offset, 4);
-        offset += 4;
+        Buffer.BlockCopy(spaceSeparator, 0, message, offset, spaceSeparator.Length);
+        offset += spaceSeparator.Length;
 
-        // Ключ
         Buffer.BlockCopy(keyBytes, 0, message, offset, keyBytes.Length);
         offset += keyBytes.Length;
 
-        // Длина значения (4 байта)
-        Buffer.BlockCopy(valueLen, 0, message, offset, 4);
-        offset += 4;
+        Buffer.BlockCopy(spaceSeparator, 0, message, offset, spaceSeparator.Length);
+        offset += spaceSeparator.Length;
 
-        // Значение
         Buffer.BlockCopy(value, 0, message, offset, value.Length);
 
-        await _stream.WriteAsync(message, 0, message.Length);
-        await _stream.FlushAsync();
+        var arrayPool = ArrayPool<byte>.Shared;
+        byte[] buffer = arrayPool.Rent(10);
+
+        try
+        {
+            await _stream.WriteAsync(message.AsMemory(0, message.Length), token);
+                       
+            int read = await _stream.ReadAsync(buffer.AsMemory(0, buffer.Length), token);
+
+            if (read > 0)
+            {
+                string setResult = Encoding.UTF8.GetString(buffer, 0, read);
+
+                if (setResult.StartsWith("OK"))
+                    return true;
+            }
+
+            return false;
+        }
+        finally
+        {
+            await _stream.FlushAsync(token);
+            arrayPool.Return(buffer, false);
+        }
     }
 
-    public async Task<byte[]> GetAsync(string key)
+    public async Task<string?> GetAsync(string key, CancellationToken token)
     {
         if (!_client.Connected || _stream == null)
             throw new InvalidOperationException("Соединение не установлено");
 
-        // Формируем сообщение: GET + key length + key
-        var keyBytes = Encoding.UTF8.GetBytes(key);
-        var keyLen = BitConverter.GetBytes(keyBytes.Length);
+        byte[] commandBytes = Encoding.UTF8.GetBytes("GET");
+        byte[] spaceSeparator = Encoding.UTF8.GetBytes(" ");
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
 
-        var message = new byte[4 + 4 + keyBytes.Length];
+        byte[] message = new byte[commandBytes.Length + spaceSeparator.Length + keyBytes.Length];
+
         int offset = 0;
 
-        // Команда GET (4 байта)
-        Encoding.UTF8.GetBytes("GET ", 0, 4, message, offset);
-        offset += 4;
+        Buffer.BlockCopy(commandBytes, 0, message, offset, commandBytes.Length);
+        offset += commandBytes.Length;
 
-        // Длина ключа (4 байта)
-        Buffer.BlockCopy(keyLen, 0, message, offset, 4);
-        offset += 4;
+        Buffer.BlockCopy(spaceSeparator, 0, message, offset, spaceSeparator.Length);
+        offset += spaceSeparator.Length;
 
-        // Ключ
         Buffer.BlockCopy(keyBytes, 0, message, offset, keyBytes.Length);
 
-        await _stream.WriteAsync(message, 0, message.Length);
-        
-        byte[] buffer = new byte[1024];
+        var arrayPool = ArrayPool<byte>.Shared;
+        byte[] buffer = arrayPool.Rent(1024);
 
-        await _stream.ReadAsync(buffer, 0, buffer.Length);
-        
-        return buffer;
+        try
+        {
+            await _stream.WriteAsync(message.AsMemory(0, message.Length), token);
+
+            int read = await _stream.ReadAsync(buffer.AsMemory(0, buffer.Length), token);
+
+            if(read > 0)
+            {
+                string getResult = Encoding.UTF8.GetString(buffer, 0, read);
+
+                if (getResult.StartsWith("(nil)\r\n"))
+                    return null;
+
+                return getResult;
+            }
+
+            return null;
+        }
+        finally
+        {
+            await _stream.FlushAsync(token);
+            arrayPool.Return(buffer, false);
+        }
     }
-    
+
     public void Dispose()
     {
         _stream?.Dispose();
         _client?.Dispose();
+
+        GC.SuppressFinalize(this);
     }
 }
